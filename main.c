4bbbb6d1fa7b58ca64b4b3f5401abd38705385d9
sophie liu <81523986+SophieL27@users.noreply.github.com>
2023-07-13 13:21:57 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n=1;
+   while(!((n%2==1)&&(n%3==2)&&(n%5==4)&&(n%6==5)&&(n%7==0)))
+   {
+       n=n+1;
+   }
+   printf("%d",n);
+    return 0;
+}
